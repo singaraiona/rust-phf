ee6fd46478f4e15c887bbf01d703dcea135dfeb0
Anton <singaraiona@gmail.com>
2020-03-11 16:34:24 +0200

    wip

--- phf_macros/src/lib.rs
+++ phf_macros/src/lib.rs
@@ -125,7 +125,7 @@ impl ParsedKey {
             }
             Expr::Group(group) => ParsedKey::from_expr(&group.expr),
             Expr::Type(c) => Some(ParsedKey::U32(124)),
-            Expr::Path(c) => Some(ParsedKey::U32(124)),
+            // Expr::Path(c) => Some(ParsedKey::U32(124)),
             _ => None,
         }
     }
